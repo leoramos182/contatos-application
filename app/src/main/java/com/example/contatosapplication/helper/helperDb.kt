8f328ea25a2c41898e6d2f16bed9560db9e2795b
leoramos182 <leo.ramos182@hotmail.com>
2021-03-06 15:52:56 -0300

    Criando db helper

--- app/src/main/java/com/example/contatosapplication/helper/helperDb.kt
+++ app/src/main/java/com/example/contatosapplication/helper/helperDb.kt
@@ -1,2 +1,27 @@
 package com.example.contatosapplication.helper
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class helperDb(
+    context: Context
+) : SQLiteOpenHelper(context, NOME_APP, null, VERSAO_ATUAL) {
+    companion object {
+        val NOME_APP = "CONTATOS_APP"
+        val VERSAO_ATUAL = 1
+
+        val TABLE_NAME = "contatos"
+        val DROP_TABLE = "DROP TABLE IF EXISTS $NOME_APP"
+    }
+    override fun onCreate(p0: SQLiteDatabase?) {
+    }
+
+    override fun onUpgrade(db : SQLiteDatabase?, oldVersion : Int, newVersion : Int) {
+        if(oldVersion != newVersion){
+            db?.execSQL(DROP_TABLE)
+        }
+        onConfigure(db)
+    }
+
+}
